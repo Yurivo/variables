47d6020e5d2e5889bc0dc6ffd4b66c9f48395603
Yuriy Vorobyev <42265532+Yurivo@users.noreply.github.com>
2024-12-09 16:38:42 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,4 @@
+course_name = 'Python'
+conpleted_dz = 12
+hours = 1.5
+print('Курс:', str(course_name) + ',', 'всего задач:' , str(conpleted_dz) + ',' , 'затрачено часов:' , str(hours) + ',' , 'среднее время выполнениия' , hours / conpleted_dz )
